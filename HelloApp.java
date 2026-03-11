a3069fdcd7f312bec221b9119e66ba26a3e6a5cd
rshreyamenon-10 <rshreyamenon@gmail.com>
2026-03-11 21:33:15 +0530

    Code to Diplay Hello using Commandline Arguments

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,11 @@
 public class HelloApp {
-      public static void main(String[] args){
-           System.out.println("Hello, World!");
-      }
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello World!");
+            System.out.println("Usage: java HelloApp <your_name>");
+        }
+    }
 }
-
