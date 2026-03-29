af12cac3a6485e694fe3cb495fd8050e7aceaa0f
rshreyamenon-10 <rshreyamenon@gmail.com>
2026-03-29 18:15:08 +0530

    Code for UC-6

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,20 @@
 public class HelloApp {
-      public static void main(String[] args){
-           System.out.println("Hello, World!");
-      }
-}
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder nameBuilder = new StringBuilder();
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
 
+            String finalNames = nameBuilder.toString();
+
+            if (finalNames.length() > 0) {
+                finalNames = finalNames.substring(0, finalNames.length() - 2);
+            }
+
+            System.out.println("Hello, " + finalNames + "!");
+        }
+    }
+}
